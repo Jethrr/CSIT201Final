0a135cb2b3fd6076ddf7e10c202285af334161ce
Jether Omictin <jethomictin@gmail.com>
2023-11-17 19:49:21 +0800

    ADDED PARTICLES.JS

---
+++ script.js
@@ -0,0 +1,76 @@
+//THIS SECTION IS  FROM PARTICLES.JS LIBRARY SIR SO THAT I CAN USE THE PARTICLE EFFECT ;)
+particlesJS("particles", {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#000000",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      animation: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#000000",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+    },
+  },
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onHover: {
+        enable: true,
+        mode: "push",
+      },
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+    },
+  },
+  retina_detect: true,
+});
